8a47d68e9f1c2a6a18f12a7ef58e9702dd54861d
Paulo Almeida <paulo.roberto.a@hotmail.com>
2022-08-14 00:54:07 +0200

    creating button component

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,17 @@
-<script setup>
+<script>
+import Header from "./components/Header.vue";
 
+export default {
+  name: "App",
+  components: {
+    Header,
+  },
+};
 </script>
 
 <template>
-  <h1>Hello Moto Mami
-  </h1>
+<Header title="Task Tracker" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style >
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,52 @@
+<script >
+import Button from "./Button.vue";
+
+export default {
+  name: "Header",
+
+  props: {
+    title: String,
+  },
+  components: {
+    Button,
+  },
+};
+</script>
+
+<template>
+  <header class="header">
+    <div>
+      <div
+        class="
+          bg-white
+          shadow-md
+          border border-gray-200
+          rounded-lg
+          max-w-sm
+          mb-5
+        "
+      >
+        <div class="p-5 flex justify-between ">
+          <a href="#">
+            <h5 class="text-gray-900 font-bold text-2xl tracking-tight mb-2">
+              {{ title }}
+            </h5>
+          </a>
+          <Button />
+          </div>
+          <div class="p-5">
+          <p class="font-normal text-gray-700 mb-3">
+            Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order.
+          </p>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+.header {
+  @apply mt-5 max-w-lg mx-auto;
+}
+</style>
